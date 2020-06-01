01fc4743cb888ec7a879ca8f75249b9c08418715
noahShafer <noahshafer0211@gmail.com>
2020-06-01 16:25:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.querySelector('#create-task-form');
+  let taskField = document.querySelector('#new-task-description');
+  let tasksList = document.querySelector('#tasks');
+
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newTask = document.createElement('li');
+    newTask.innerHTML = taskField.value;
+    tasksList.appendChild(newTask);
+    taskField.value = '';
+  });
+  
+
+
 });
